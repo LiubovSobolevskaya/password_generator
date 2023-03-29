5684610465b2b34732a2990a55116c471e8acc2c
Liubov Sobolevskaya <luba@Liubovs-MBP-2.attlocal.net>
2023-03-29 16:12:02 -0700

    make charackter positions random

--- README.md
+++ README.md
@@ -11,7 +11,49 @@ __________________________
 
 For the list of special characters I refered to [Wikipedia]( https://en.wikipedia.org/wiki/List_of_special_characters_for_passwords). For this reason, space is included to the list of special charaters. 
 
+```javascript
+var specialChars = "~!?@ .|#;$%^&<>,[]=`_*():/+-'\"\\";
+```
 __________________________
+## Code Example
+```javascript
+function randomGenerator(charactersToInclude, passwordLength){
+  
+  var nCharacterTypes = charactersToInclude.length;
+
+  var generatedPassword = "";
+
+  var lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var specialChars = "~!?@ .|#;$%^&<>,[]=`_*():/+-'\"\\";
+  
+  var i = 0
+
+  while (i < passwordLength){
+    var selectedIdx = createRndNmr(nCharacterTypes)
+    var selectedCharacterType = charactersToInclude[selectedIdx];
+    switch (selectedCharacterType){
+      case "lowercase":
+        generatedPassword += lowerCaseChars.charAt(createRndNmr(lowerCaseChars.length));
+        break;
+      case "uppercase":
+        generatedPassword += upperCaseChars.charAt(createRndNmr(upperCaseChars.length));
+        break;    
+      case "numeric values":
+        generatedPassword += createRndNmr(10);
+        break;
+      case "special characters":
+        generatedPassword += specialChars.charAt(createRndNmr(specialChars.length));
+        break;
+    }
+    i++;    
+  }
+  
+  assert(generatedPassword.length == passwordLength);
+
+  return generatedPassword;
+}
+```
 ## Technology Used 
 
 * [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)
--- assets/script.js
+++ assets/script.js
@@ -38,46 +38,47 @@ function randomGenerator(charactersToInclude, passwordLength){
     i++;    
   }
 
-   
   assert(generatedPassword.length == passwordLength);
 
   return generatedPassword;
 }
 
 
-function generatePassword()
-{
-    var passwordLength = prompt("Please enter the length of the password");
-    if (passwordLength != null) {
-      if (passwordLength < 8){
-        alert("Password needs to be at least 8 symbols!");
-        return "";
-      }
-      else if (passwordLength > 128){
-        alert("Password needs to be at most 128 symbols!");
-        return "";
-      }
-      else{
-        var characterOptions = ["lowercase", "uppercase", "numeric values", "special characters"]
-        var charactersToInclude = []
-        for (var i in characterOptions){
-          if (confirm(`Would you like you password to contain ${characterOptions[i]}?`)){
-            charactersToInclude.push(characterOptions[i])
-          }
-        }
-        if ( charactersToInclude.length === 0){
-          alert("At least one character type should be selected!");
-          return "";
-        }
-        else{
-          return randomGenerator(charactersToInclude, passwordLength);
-        }
-      }
+function randomGenerator(charactersToInclude, passwordLength){
+  
+  var nCharacterTypes = charactersToInclude.length;
 
+  var generatedPassword = "";
+
+  var lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var specialChars = "~!?@ .|#;$%^&<>,[]=`_*():/+-'\"\\";
+  
+  var i = 0
+
+  while (i < passwordLength){
+    var selectedIdx = createRndNmr(nCharacterTypes)
+    var selectedCharacterType = charactersToInclude[selectedIdx];
+    switch (selectedCharacterType){
+      case "lowercase":
+        generatedPassword += lowerCaseChars.charAt(createRndNmr(lowerCaseChars.length));
+        break;
+      case "uppercase":
+        generatedPassword += upperCaseChars.charAt(createRndNmr(upperCaseChars.length));
+        break;    
+      case "numeric values":
+        generatedPassword += createRndNmr(10);
+        break;
+      case "special characters":
+        generatedPassword += specialChars.charAt(createRndNmr(specialChars.length));
+        break;
     }
-    else{
-      return "";
-    }
+    i++;    
+  }
+  
+  assert(generatedPassword.length == passwordLength);
+
+  return generatedPassword;
 }
 
 function writePassword() {
